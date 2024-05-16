b02f59933c66e54681f17600a5ffe82837697951
Aliza Buchen <alizabuchen@alizas-air-2.mynetworksettings.com>
2024-05-16 11:46:21 -0400

    added unit test

--- src/main/java/buchen/rijks/RijksService.java
+++ src/main/java/buchen/rijks/RijksService.java
@@ -1,26 +1,26 @@
 package buchen.rijks;
 
-import io.reactivex.rxjava3.core.Observable;
+import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface RijksService {
     @GET("/api/en/collection")
-    Observable<ArtObjects> page(
-            @Query("apikey") String key,
+    Single<ArtObjects> page(
+            @Query("key") String key,
             @Query("p") int page
     );
 
     @GET("/api/en/collection")
-    Observable<ArtObjects> query(
-            @Query("apikey") String key,
+    Single<ArtObjects> query(
+            @Query("key") String key,
             @Query("p") int page,
             @Query("q") String query
     );
 
     @GET("/api/en/collection")
-    Observable<ArtObjects> artist(
-            @Query("apikey") String key,
+    Single<ArtObjects> artist(
+            @Query("key") String key,
             @Query("p") int page,
             @Query("involvedMaker") String artist
 
