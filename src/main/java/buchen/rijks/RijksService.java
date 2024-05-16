5bad95097bfc3429d1986f3d3c3411d630948cc5
Aliza Buchen <alizabuchen@alizas-air-2.mynetworksettings.com>
2024-05-16 01:24:12 -0400

    checkstyles

--- src/main/java/buchen/rijks/RijksService.java
+++ src/main/java/buchen/rijks/RijksService.java
@@ -6,20 +6,20 @@ import retrofit2.http.Query;
 
 public interface RijksService {
     @GET("/api/en/collection")
-    Observable<ArtObjects> page (
+    Observable<ArtObjects> page(
             @Query("apikey") String key,
             @Query("p") int page
     );
 
     @GET("/api/en/collection")
-    Observable<ArtObjects> query (
+    Observable<ArtObjects> query(
             @Query("apikey") String key,
             @Query("p") int page,
             @Query("q") String query
     );
 
     @GET("/api/en/collection")
-    Observable<ArtObjects> artist (
+    Observable<ArtObjects> artist(
             @Query("apikey") String key,
             @Query("p") int page,
             @Query("involvedMaker") String artist
