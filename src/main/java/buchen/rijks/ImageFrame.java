8ee34e9f65e864c5dd1ae9cc2b4854be5d421502
Aliza Buchen <alizabuchen@alizas-air-2.mynetworksettings.com>
2024-05-24 18:35:53 -0400

    fixed imageFrame

--- src/main/java/buchen/rijks/ImageFrame.java
+++ src/main/java/buchen/rijks/ImageFrame.java
@@ -5,11 +5,16 @@ import java.awt.*;
 
 public class ImageFrame extends JFrame {
     public ImageFrame(ImageIcon imageIcon, String title, String artist) {
-        setSize(800, 600);
+        setSize(700, 800);
         setTitle(title + " by " + artist);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 
-        JLabel imageLabel = new JLabel(imageIcon);
+        Image originalImage = imageIcon.getImage();
+
+        Image scaledImage = originalImage.getScaledInstance(800, -1, Image.SCALE_SMOOTH);
+        ImageIcon scaledImageIcon = new ImageIcon(scaledImage);
+
+        JLabel imageLabel = new JLabel(scaledImageIcon);
         JScrollPane scrollPane = new JScrollPane(imageLabel);
 
         add(scrollPane, BorderLayout.CENTER);
